c5b45111768978490d4c8efed8869cae50b4d285
Hans Eric Lippke <helippke@betteru.ca>
2021-11-11 19:23:42 +0000

    Added asserArraysEqual Function

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const eqArrays = function(arr1, arr2) {
+    // Check if the length of the arrays are equal
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    // Check if the elements in the arrays are equal
+    for (let i = 0; i < arr1.length; i++) {
+
+        // Check if the elements are equal
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+   
+    return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+    if (eqArrays(arr1, arr2)) {
+        console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+    } else {
+        console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+    }
+};
