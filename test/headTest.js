178d5785139adbcf850e29d7b212f8a5184389e6
Hans Eric Lippke <helippke@betteru.ca>
2021-11-25 18:07:35 +0000

    Added mocha chai and refactored headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,28 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-//Tests
-assertEqual(head([5,6,7]), 5);//number array
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");//string array
-assertEqual(head(["One"]), "One");//one element array
-assertEqual(head([]), undefined);//empty array
+describe("#head", () => {
+    it("returns 1 for [1, 2, 3]", () => {
+        assert.strictEqual(head([1, 2, 3]), 1);
+    });
+
+    it("returns '5' for ['5']", () => {
+        assert.strictEqual(head(['5']), '5');
+    });
+
+    it("returns '5' for [5, 6, 7]", () => {
+        assert.strictEqual(head([5, 6, 7]), 5);
+    });
+    
+    it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+        assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+    });
+
+    it("returns  'One' for ['One']", () => {
+        assert.strictEqual(head(['One']), 'One');
+    });
+
+    it("returns undefined for []", () => {
+        assert.strictEqual(head([]), undefined);
+    });  
+});
