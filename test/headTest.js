9cd8e1760398727e4c76354cb871b9afced030db
Hans Eric Lippke <helippke@betteru.ca>
2021-11-25 18:16:56 +0000

    Refactored middleTest and headTest to use mocha chai with expect

--- test/headTest.js
+++ test/headTest.js
@@ -1,28 +1,34 @@
-const assert = require('chai').assert;
+const {expect} = require('chai');
 const head   = require('../head');
 
 describe("#head", () => {
     it("returns 1 for [1, 2, 3]", () => {
-        assert.strictEqual(head([1, 2, 3]), 1);
+        const output = head([1, 2, 3]);
+        expect(output).to.equal(1);
     });
 
     it("returns '5' for ['5']", () => {
-        assert.strictEqual(head(['5']), '5');
+        const output = head(['5']);
+        expect(output).to.equal('5');
     });
 
     it("returns '5' for [5, 6, 7]", () => {
-        assert.strictEqual(head([5, 6, 7]), 5);
+        const output = head([5, 6, 7]);
+        expect(output).to.equal(5);
     });
     
     it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
-        assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+        const output = head(['Hello', 'Lighthouse', 'Labs']);
+        expect(output).to.equal('Hello');
     });
 
     it("returns  'One' for ['One']", () => {
-        assert.strictEqual(head(['One']), 'One');
+        const output = head(['One']);
+        expect(output).to.equal('One');
     });
 
     it("returns undefined for []", () => {
-        assert.strictEqual(head([]), undefined);
+        const output = head([]);
+        expect(output).to.equal(undefined);
     });  
 });
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,34 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const {expect} = require('chai');
 const middle = require('../middle');
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+describe("#middle", () => {
+    it("returns [] for [1]", () => {
+        const output = middle([1]);
+        expect(output).to.eql([]);
+    });
+
+    it("returns [] for [1, 2]", () => {
+        const output = middle([1, 2]);
+        expect(output).to.eql([]);
+    });
+
+    it("returns [2] for [1, 2, 3]]", () => {
+        const output = middle([1, 2, 3]);
+        expect(output).to.eql([2]);
+    });
+    
+    it("returns [2, 3] for [1, 2, 3, 4]", () => {
+        const output = middle([1, 2, 3, 4]);
+        expect(output).to.eql([2, 3]);
+    });
+
+    it("returns [3] for [1, 2, 3, 4, 5]", () => { 
+        const output = middle([1, 2, 3, 4, 5]);
+        expect(output).to.eql([3]);
+    });
+
+    it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+        const output = middle([1, 2, 3, 4, 5, 6]);
+        expect(output).to.eql([3, 4]);
+    });
+});
