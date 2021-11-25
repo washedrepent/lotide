0cfb69a040b60a578632e0e2d7ed5b739efc29d2
Hans Eric Lippke <helippke@betteru.ca>
2021-11-25 19:16:07 +0000

    Exported all files as one object

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const head = require('./head');
+const map = require('./map');
+const middle = require('./middle');
+
+module.exports = {
+    assertArraysEqual,
+    assertEqual,
+    assertObjectsEqual,
+    countOnly,
+    eqArrays,
+    eqObjects,
+    findKey,
+    findKeyByValue,
+    head,
+    map,
+    middle
+}
